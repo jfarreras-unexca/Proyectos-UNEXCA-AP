8b0e2e595812d1b0231e191f6c7406246c1c3fcc
Jose Ángel <jfarrerasunexca@gmail.com>
2024-11-08 19:52:18 -0400

    crear carpeta "ejercicios_vectores" de AyP, creacion de carpeta para usuario "jperez".
    Desarrollo de programa "suma_vector_A_B.cpp"

---
+++ ejercicios_vectores/jperez/suma_vector_A_B.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int A = 0;
+int B = 0;
+int Resp = 0;
+
+
+
+int main()
+
+{
+    //	system("clear");
+
+	cout << "Coloca el Primer Numero =";
+	cin >> A;
+
+	cout << "Coloca el Segundo Numero =";
+	cin >> B;
+
+	Resp = A+B;
+	cout << "El Resultado Es =" << Resp;
+	return (0);
+
+}
