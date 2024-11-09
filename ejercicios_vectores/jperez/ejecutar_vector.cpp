7451ff810b93ce4ad51073d0272c3908d8d33dfe
Jose Ángel <jfarrerasunexca@gmail.com>
2024-11-08 20:42:20 -0400

    ejercicio de vectores

---
+++ ejercicios_vectores/jperez/ejecutar_vector.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int vector[5];
+int I = 0;
+
+int main()
+{
+system ("cls");
+
+for (I = 0; I <= 4; I++)
+{
+
+	cout << " Inrese la posicion ";
+	cin >> vector[I];
+	cout << vector[I];
+
+}
+
+}
---
+++ ejercicios_vectores/jperez/ejecutar_vector_V1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int vector[5];
+int I = 0;
+int n = 0;
+
+
+int main()
+{
+system ("cls");
+
+for (I = 0; I <= n; I++)
+{
+
+    cout << "Ingrese el tamaño del vector ";
+    cin >> n;
+
+	cout << " Inrese la posicion ";
+	cin >> vector[I];
+
+	cout << vector[I];
+
+}
+
+}
